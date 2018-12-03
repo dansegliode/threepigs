ab7c1e910521f3c9e4b6390950c4d5dea546948b
Andrew Hurley <hurleymandrew@gmail.com>
2018-12-03 02:45:34 -0500

    more comments

--- js/pig2wolfattack.js
+++ js/pig2wolfattack.js
@@ -1,21 +1,29 @@
 // radar animation function
+// plays a radar animation at a user defined x y and with a user defined color
+
 function playRadar(x, y, color) {
+	// sets up two variables for later use with adjustments so they are more spot on
 
     let xPos = x;
     let yPos = y;
     console.log(xPos + " " + yPos);
+	// adds html to the document that is the radar html
+
     $("body").prepend(
         '	<div id="radarEffect"><div id="functionBasedDelay"><div class="line"><div class="beam am"><div class="beam am"><div class="beam am"></div></div></div></div></div></div>'
     );
+	// adds the neccessary css to the beam so the color is correct
 
     $(".beam").css('box-shadow', '0px 0px 5px ' + `${color}`);
     $(".beam").css('border', '2px solid ' + `${color}`);
+	// places the beams in the right location absolute to the window
 
     $("#radarEffect").css({
         position: "absolute",
         left: xPos,
         top: yPos
     });
+	// anime.js function based delay for animating the beams outward
 
     var functionBasedDelay = anime({
         targets: '#functionBasedDelay .am ',
@@ -28,14 +36,18 @@ function playRadar(x, y, color) {
             return i * 100;
         }
     });
+	// waiting time until the script deletes the beam allowing for another to be played
 
     setTimeout(function () {
+		// grabs the body document because the radar effect is prepended it is a child and can be deleted with removeChild function
+
         var element = document.body;
         var child = document.getElementById("radarEffect");
         element.removeChild(child);
     }, 1000);
 
 }
+// scaling animation for the object being picked up
 
 function scaleAnimation(obj) {
     $(`${obj}`).addClass('boing');
@@ -62,12 +74,13 @@ function playScatter() {
     piece2.addClass("staticMat2Transition");
     piece3.addClass("staticMat3Transition");
 }
-
+// adding class names for the animation of all the fades used on this page
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById('fade1').className += ' fade';
     document.getElementById('fade2').className += ' fade';
     document.getElementById('fade3').className += ' fade';
 });
+// removing the fades on completion
 
 $(function () {
     $('#fade1').removeClass('fade');
