25ae22d8e3984fd856e9a673d8ffb493ef4a057d
Sarah Danseglio <sarahdanseglio@Sarahs-MacBook-Pro-3.local>
2018-12-02 21:46:07 -0500

    beautified all files and separated JS

---
+++ js/pig2wolf.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById('fade1').className += ' fade';
+    document.getElementById('fade6').className += ' fade';
+    $('.whiteText').addClass('storyFadeOut');
+});
+
+$(function () {
+    $('#fade1').removeClass('fade');
+    $('#fade6').removeClass('fade');
+});
+
+setTimeout(function () {
+    document.addEventListener("wheel", function (e) {
+        $('.storyPara').removeClass('storyFadeOut');
+        e.preventDefault();
+        $link = $("body").attr("href");
+        $(".sceneIntroContainer").fadeOut(500, function () {
+            window.location = $link;
+        });
+    });
+}, 2700);
