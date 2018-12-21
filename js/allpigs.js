1f08b2394f6de570b079a42a9e8e801f3aad570f
Andrew Hurley <hurleymandrew@gmail.com>
2018-12-21 00:52:42 -0500

    first page firefox functionality

--- js/allpigs.js
+++ js/allpigs.js
@@ -83,9 +83,11 @@ let hayX;
 let hayY;
 let bricksPig, haysPig, sticksPig;
 let hayAnimationPlayed = false,
-	stickAnimationPlayed = false,
-	brickAnimationPlayed = false;
+stickAnimationPlayed = false,
+brickAnimationPlayed = false;
 //  grabbing the material coordinates by accessing their DOM attributes, first it grabs the object by its ID with jquery then goes to the first attribute which is array position 0 then accessed their offsets because they are absolute to the page and are the best representation of true coordinates
+let isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
 function updateMatCoords() {
 	hayX = $("#hay1")[0].offsetLeft;
 	hayY = $("#hay1")[0].offsetTop;
@@ -115,10 +117,13 @@ var img = new Image();
 img.src = 'imgs/blank.png';
 document.addEventListener("dragstart", function (event) {
 	updateMatCoords();
+	if (isFirefox) {
+		pigTarget = "#" + event.target.parentNode.id;
+	}else{
+		pigTarget = "#" + event.path[1].id;
+	}
 
-	pigTarget = "#" + event.path[1].id;
 	event.dataTransfer.setData("text/plain", event.target.id);
-
 	event.dataTransfer.setDragImage(img, 0, 0);
 	// on drag it does the scale animation to the current target object
 	scaleAnimation(pigTarget);
@@ -169,91 +174,91 @@ document.addEventListener("drag", function (event) {
 		if (!hayTargetFilled) {
 			// then if the cursor is in a certain location reelative to the filled checker object
 			if (((mouseXPos > (hayX - 25)) && (mouseXPos < (hayX + 125))) && ((mouseYPos > (hayY - 25)) && (mouseYPos < (
-					hayY + 125)))) {
-				// sets that object to snap into place of the old object
-				$(`${pigTarget}`).css({
-					position: "absolute",
-					left: hayX,
-					top: hayY
-				});
-				// animates on snap
-				scaleAnimation(pigTarget);
+				hayY + 125)))) {
+					// sets that object to snap into place of the old object
+					$(`${pigTarget}`).css({
+						position: "absolute",
+						left: hayX,
+						top: hayY
+					});
+					// animates on snap
+					scaleAnimation(pigTarget);
+				}
 			}
 		}
-	}
 
-	// updates coordinates of a material, selected based on its DOM attributes offset
-	stickX = $("#stick1")[0].offsetLeft;
-	stickY = $("#stick1")[0].offsetTop;
-	if ((pigTarget != "#hay1") && (pigTarget != "#stick1") && (pigTarget != "#brick1")) {
-		if (!stickTargetFilled) {
-			if (((mouseXPos > (stickX - 25)) && (mouseXPos < (stickX + 125))) && ((mouseYPos > (stickY - 25)) && (mouseYPos <
+		// updates coordinates of a material, selected based on its DOM attributes offset
+		stickX = $("#stick1")[0].offsetLeft;
+		stickY = $("#stick1")[0].offsetTop;
+		if ((pigTarget != "#hay1") && (pigTarget != "#stick1") && (pigTarget != "#brick1")) {
+			if (!stickTargetFilled) {
+				if (((mouseXPos > (stickX - 25)) && (mouseXPos < (stickX + 125))) && ((mouseYPos > (stickY - 25)) && (mouseYPos <
 					(stickY + 125)))) {
-				$(`${pigTarget}`).css({
-					position: "absolute",
-					left: stickX,
-					top: stickY
-				});
-				scaleAnimation(pigTarget);
+						$(`${pigTarget}`).css({
+							position: "absolute",
+							left: stickX,
+							top: stickY
+						});
+						scaleAnimation(pigTarget);
+					}
+				}
 			}
-		}
-	}
 
-	brickX = $("#brick1")[0].offsetLeft;
-	brickY = $("#brick1")[0].offsetTop;
-	if ((pigTarget != "#hay1") && (pigTarget != "#stick1") && (pigTarget != "#brick1")) {
-		if (!brickTargetFilled) {
-			if (((mouseXPos > (brickX - 25)) && (mouseXPos < (brickX + 125))) && ((mouseYPos > (brickY - 25)) && (mouseYPos <
+			brickX = $("#brick1")[0].offsetLeft;
+			brickY = $("#brick1")[0].offsetTop;
+			if ((pigTarget != "#hay1") && (pigTarget != "#stick1") && (pigTarget != "#brick1")) {
+				if (!brickTargetFilled) {
+					if (((mouseXPos > (brickX - 25)) && (mouseXPos < (brickX + 125))) && ((mouseYPos > (brickY - 25)) && (mouseYPos <
+						(brickY + 125)))) {
+							$(`${pigTarget}`).css({
+								position: "absolute",
+								left: brickX,
+								top: brickY
+							});
+							scaleAnimation(pigTarget);
+						}
+					}
+				}
+			});
+
+			document.addEventListener("dragend", function (event) {
+
+				updateMatCoords();
+
+				console.log("drag end");
+				// when the drag ends it sees where the mouse is, if it is on a certain object, and the checks if all the objects are in their spot
+				if (((mouseXPos > (brickX - 25)) && (mouseXPos < (brickX + 125))) && ((mouseYPos > (brickY - 25)) && (mouseYPos <
 					(brickY + 125)))) {
-				$(`${pigTarget}`).css({
-					position: "absolute",
-					left: brickX,
-					top: brickY
-				});
-				scaleAnimation(pigTarget);
-			}
-		}
-	}
-});
-
-document.addEventListener("dragend", function (event) {
-
-	updateMatCoords();
-
-	console.log("drag end");
-	// when the drag ends it sees where the mouse is, if it is on a certain object, and the checks if all the objects are in their spot
-	if (((mouseXPos > (brickX - 25)) && (mouseXPos < (brickX + 125))) && ((mouseYPos > (brickY - 25)) && (mouseYPos <
-			(brickY + 125)))) {
-		// if it is the object it is checking for it sets its target to be filled
-		brickTargetFilled = true;
-		// it then sets the one object to the current object
-		bricksPig = pigTarget;
-		// sees which object equals the dropped object
-		if (bricksPig == "#pig1") {
-			// if this happens to be the object it sets its draggability to false
-			pig1Draggable = false;
-		} else if (bricksPig == "#pig2") {
-			pig2Draggable = false;
-		} else if (bricksPig == "#pig3") {
-			pig3Draggable = false;
-		}
-		// checks if the end is true
-		checkForEnd();
-	}
-	// checks the rest of the targets
-	if (((mouseXPos > (stickX - 25)) && (mouseXPos < (stickX + 125))) && ((mouseYPos > (stickY - 25)) && (mouseYPos <
-			(stickY + 125)))) {
-		stickTargetFilled = true;
-		sticksPig = pigTarget;
-		if (sticksPig == "#pig1") {
-			pig1Draggable = false;
-		} else if (sticksPig == "#pig2") {
-			pig2Draggable = false;
-		} else if (sticksPig == "#pig3") {
-			pig3Draggable = false;
-		}
-		checkForEnd();
-	}
+						// if it is the object it is checking for it sets its target to be filled
+						brickTargetFilled = true;
+						// it then sets the one object to the current object
+						bricksPig = pigTarget;
+						// sees which object equals the dropped object
+						if (bricksPig == "#pig1") {
+							// if this happens to be the object it sets its draggability to false
+							pig1Draggable = false;
+						} else if (bricksPig == "#pig2") {
+							pig2Draggable = false;
+						} else if (bricksPig == "#pig3") {
+							pig3Draggable = false;
+						}
+						// checks if the end is true
+						checkForEnd();
+					}
+					// checks the rest of the targets
+					if (((mouseXPos > (stickX - 25)) && (mouseXPos < (stickX + 125))) && ((mouseYPos > (stickY - 25)) && (mouseYPos <
+						(stickY + 125)))) {
+							stickTargetFilled = true;
+							sticksPig = pigTarget;
+							if (sticksPig == "#pig1") {
+								pig1Draggable = false;
+							} else if (sticksPig == "#pig2") {
+								pig2Draggable = false;
+							} else if (sticksPig == "#pig3") {
+								pig3Draggable = false;
+							}
+							checkForEnd();
+						}
 
 						if (((mouseXPos > (hayX - 25)) && (mouseXPos < (hayX + 125))) && ((mouseYPos > (hayY - 25)) && (mouseYPos < (hayY +
 							125)))) {
@@ -271,44 +276,44 @@ document.addEventListener("dragend", function (event) {
 
 							console.log("haytarget" + hayTargetFilled);
 
-	// when an object is filled in it sees which object it is then plays the radar effect at that objects location
-	if (hayTargetFilled && !hayAnimationPlayed) {
-		playRadar(hayX, hayY, "#FFA800");
-		hayAnimationPlayed = true;
+							// when an object is filled in it sees which object it is then plays the radar effect at that objects location
+							if (hayTargetFilled && !hayAnimationPlayed) {
+								playRadar(hayX, hayY, "#FFA800");
+								hayAnimationPlayed = true;
 
 
-	}
-	if (stickTargetFilled && !stickAnimationPlayed) {
-		playRadar(stickX, stickY, "#4EF24B");
-		stickAnimationPlayed = true;
+							}
+							if (stickTargetFilled && !stickAnimationPlayed) {
+								playRadar(stickX, stickY, "#4EF24B");
+								stickAnimationPlayed = true;
 
 
-	}
-	if (brickTargetFilled && !brickAnimationPlayed) {
-		playRadar(brickX, brickY, "#DF5542");
-		brickAnimationPlayed = true;
+							}
+							if (brickTargetFilled && !brickAnimationPlayed) {
+								playRadar(brickX, brickY, "#DF5542");
+								brickAnimationPlayed = true;
 
 
-	}
+							}
 
-});
-
-
-// function to check if all objects are filled in
-// function used at the end of a scene to transition out
-function checkForEnd() {
-
-	if ((pig1Draggable == false) && (pig2Draggable == false) && (pig3Draggable == false)) {
-		// if they are filled in, then it sets the href to a link variable and after a fade it sends the user to that href
-
-		console.log("finished");
-		setTimeout(function () {
-			$(".pigsAndMaterials").fadeOut(500, function () {
-				// tells the browser to go to the new location
-				window.location = "scene_pig1story.html";
-			});
-		}, 600)
+						});
 
 
-	}
-}
+						// function to check if all objects are filled in
+						// function used at the end of a scene to transition out
+						function checkForEnd() {
+
+							if ((pig1Draggable == false) && (pig2Draggable == false) && (pig3Draggable == false)) {
+								// if they are filled in, then it sets the href to a link variable and after a fade it sends the user to that href
+
+								console.log("finished");
+								setTimeout(function () {
+									$(".pigsAndMaterials").fadeOut(500, function () {
+										// tells the browser to go to the new location
+										window.location = "scene_pig1story.html";
+									});
+								}, 600)
+
+
+							}
+						}
